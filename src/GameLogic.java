4466da82e00e2076a33963e4a2429cbd95247157
danth343 <danth343@li0-5.ida.liu.se>
2016-03-11 13:08:52 +0100

    I have started to experiment with jbox2D

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,3 +1,12 @@
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+
 public class GameLogic
 {
+
 }
