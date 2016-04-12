ff73773230135fe017b250b3366bfdacaf387d65
Kristian Sikiric <kristiansikiric@c-127-195.eduroam.liu.se>
2016-04-12 10:53:13 +0200

    Added javadoc in class coin

--- src/Coin.java
+++ src/Coin.java
@@ -7,24 +7,51 @@ import org.jbox2d.dynamics.contacts.Contact;
 /**
  * Created by kristiansikiric on 2016-04-09.
  */
+
+/**
+ * This class creates a coin that can be picked up by the player. The coin contains points
+ * given to the player when picked up.
+ */
 public class Coin extends Circle implements CollisionListener {
     private int points;
     private int ID;
     private World world;
 
+    /**
+     * Creates a circle that will symbolize the coin.
+     * @param world The world where the coin will be created
+     * @param pos   The position of the coin
+     * @param friction  The friction of the coin
+     * @param image     The iamge of the coin
+     * @param ID    The ID of the coin
+     */
     public Coin(World world, Vec2 pos, float friction, Image image, int ID) {
         super(world, pos, friction, image);
         points = 100;
-        body.getFixtureList().setSensor(true);
-    }
-
-    public Coin(World world, Vec2 pos, float friction, Color color, double radious, int ID) {
-        super(world, pos, friction, color, radious);
-        points = 100;
-        body.getFixtureList().setSensor(true);
+        body.getFixtureList().setSensor(true); //Makes the coin a sensor
         body.setUserData(this);
     }
 
+    /**
+     * Creates a circle that will symbolize the coin
+     * @param world The world where the coin will be created
+     * @param pos   The posoistion of the coin
+     * @param friction  The friction of the coin
+     * @param color     The color of the coin
+     * @param radious   The radious of the coin
+     * @param ID    The ID of the coin
+     */
+    public Coin(World world, Vec2 pos, float friction, Color color, double radious, int ID) {
+        super(world, pos, friction, color, radious);
+        points = 100;
+        body.getFixtureList().setSensor(true); //Makes the coin a sensor
+        body.setUserData(this);
+    }
+
+    /**
+     * This method checks if the player have collided whit the coin, if it has the player is given the points held
+     * by the coin and the coin is then removed from the world.
+     */
     public void beginContact(Contact contact){
         if (contact.getFixtureA().getBody().getUserData().equals(this) && contact.getFixtureB().getBody().getUserData() instanceof Player){
             System.out.println("Ja");
@@ -43,12 +70,28 @@ public class Coin extends Circle implements CollisionListener {
             map.removeDrawAndUpdateObject(this);
         }
     }
+
+    /**
+     * Not needed in this case.
+     * @param contact
+     */
     public void endContact(Contact contact){}
 
+    /**
+     * @return the points held by the coin
+     */
     public int getPoints() {return points;}
 
+    /**
+     * @return the ID of the coin
+     */
     public int getID() {return ID;}
 
+    /**
+     * Overrides the method 'equals.()'
+     * @param obj The object that should be evaluated
+     * @return boolean
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Coin && ((Coin) obj).getID() == this.getID()){return true;}
